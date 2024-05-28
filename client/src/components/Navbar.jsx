881e47a51ca6c5273f4cd87bb105a3b097e3bed4
Yusuf0803 <baeyusuf094@gmail.com>
2024-05-28 17:35:51 +0700

    Styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return (
-        <nav>
+        <nav className="navbar">
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
         </nav>
     )
 }
+
