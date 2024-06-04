a366acb6fb001c243508d0798d18c80a39827977
Galang Arsandy <galangarsandy@gmail.com>
2024-06-04 17:53:04 +0700

    Create: Utils for handling upload image user pprofile

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api", require("./routes"));
