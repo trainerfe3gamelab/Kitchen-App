28d5c7bbc57ca162eb5cfa151f91de7c84d2de01
Yusuf0803 <baeyusuf094@gmail.com>
2024-06-08 22:27:28 +0700

    (revisi)Mengambil data resep berdasarkan preferensi user dan mengubah endpoint

--- server/controllers/foryouController.js
+++ server/controllers/foryouController.js
@@ -2,9 +2,23 @@ const Recipe = require('../models/recipe');
 
 const foryouController = async (req, res) => {
     try {
-        const { user_id } = req.query;
-      
-        const recipes = await Recipe.find({ user_id });
+        const { user_id, category } = req.query;
+
+        let query = {};
+
+        // If user_id is given, add it to the query
+        if (user_id) {
+            query.user_id = user_id;
+        }
+
+        // If category is given, add it to the query
+        if (category) {
+            const categories = category.split(',');
+            query.category = { $in: categories };
+        }
+
+        // Find recipes that match the query
+        const recipes = await Recipe.find(query);
 
         res.status(200).json(recipes);
     } catch (error) {
