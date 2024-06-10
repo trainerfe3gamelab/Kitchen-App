78d3391844abf50229a69cc8d92363c9e13310cc
szaaahwa <139883053+szaaahwa@users.noreply.github.com>
2024-06-10 14:27:38 +0700

    create Profile dan CardProfile component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import Register from "./pages/Register";
 import Search from "./pages/Search";
 import Recipe from "./pages/Recipe";
 import NotFound from "./pages/NotFound";
+import EditProfile from "./pages/EditProfile";
 import Profile from "./pages/Profile";
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
@@ -32,6 +33,7 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route path="/recipe/:id" element={<Recipe />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/edit-profile" element={<EditProfile/>}/>
         <Route path="/profile" element={<Profile/>}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,221 +1,184 @@
-import { useState } from "react"
-import toast, { Toaster } from 'react-hot-toast';
-
-const notify = () => toast('Data Profile Berhasil disimpan!');
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CardProfile from '../components/common/CardProfile';
 
 const Profile = () => {
+   const  data_kartu={
+        "pertama":[{
+            "nama":"Martin Joseph",
+            "image" : "/profile-cards/image 1.png",
+            "user" : "/profile-cards/Ellipse 1.png",
+            "time": "30m",
+            "likes":"10k",
+            "title":"Martabak Manis Cokelat Kacang Teflon",
+        }],
+        "kedua":[{
+            "nama":"Bella Haddad",
+            "image" : "/profile-cards/image 2.png",
+            "user" : "/profile-cards/Ellipse 2.png",
+            "time": "1,5j",
+            "likes":"2.3k",
+            "title":"Resep Opor Ayam Yang Mantap Untuk Lebaran",
+        }],
+        "ketiga":[{
+            "nama":"Ruth Stewart",
+            "image" : "/profile-cards/image 3.png",
+            "user" : "/profile-cards/Ellipse 3.png",
+            "time": "30 m",
+            "likes":"2.3k",
+            "title":"Resep Sop Buah Mutiara, Minuman Dingin yang Cocok untuk Musim Panas",
+        }],
+        "empat":[{
+            "nama":"Marelyn Johanson",
+            "image" : "/profile-cards/image 4.png",
+            "user" : "/profile-cards/Ellipse 4.png",
+            "time": "30 m",
+            "likes":"2.3k",
+            "title":"Resep Babi Kurma By LastHope Kitchen, Bikin Masuk Penjara",
+        }],
+        "kelima":[{
+            "nama":"Martin Joseph",
+            "image" : "/profile-cards/image 1.png",
+            "user" : "/profile-cards/Ellipse 1.png",
+            "time": "30m",
+            "likes":"10k",
+            "title":"Martabak Manis Cokelat Kacang Teflon",
+        }],
+        "enam":[{
+            "nama":"Bella Haddad",
+            "image" : "/profile-cards/image 2.png",
+            "user" : "/profile-cards/Ellipse 2.png",
+            "time": "1,5j",
+            "likes":"2.3k",
+            "title":"Resep Opor Ayam Yang Mantap Untuk Lebaran",
+        }],
+        "tujuh":[{
+            "nama":"Ruth Stewart",
+            "image" : "/profile-cards/image 3.png",
+            "user" : "/profile-cards/Ellipse 3.png",
+            "time": "30 m",
+            "likes":"2.3k",
+            "title":"Resep Sop Buah Mutiara, Minuman Dingin yang Cocok untuk Musim Panas",
+        }],
+        "delapan":[{
+            "nama":"Marelyn Johanson",
+            "image" : "/profile-cards/image 4.png",
+            "user" : "/profile-cards/Ellipse 4.png",
+            "time": "30 m",
+            "likes":"2.3k",
+            "title":"Resep Babi Kurma By LastHope Kitchen, Bikin Masuk Penjara",
+        }],
+        "9":[{
+            "nama":"Martin Joseph",
+            "image" : "/profile-cards/image 1.png",
+            "user" : "/profile-cards/Ellipse 1.png",
+            "time": "30m",
+            "likes":"10k",
+            "title":"Martabak Manis Cokelat Kacang Teflon",
+        }],
+        "10":[{
+            "nama":"Bella Haddad",
+            "image" : "/profile-cards/image 2.png",
+            "user" : "/profile-cards/Ellipse 2.png",
+            "time": "1,5j",
+            "likes":"2.3k",
+            "title":"Resep Opor Ayam Yang Mantap Untuk Lebaran",
+        }],
+        "11":[{
+            "nama":"Ruth Stewart",
+            "image" : "/profile-cards/image 3.png",
+            "user" : "/profile-cards/Ellipse 3.png",
+            "time": "30 m",
+            "likes":"2.3k",
+            "title":"Resep Sop Buah Mutiara, Minuman Dingin yang Cocok untuk Musim Panas",
+        }],
+        "12":[{
+            "nama":"Marelyn Johanson",
+            "image" : "/profile-cards/image 4.png",
+            "user" : "/profile-cards/Ellipse 4.png",
+            "time": "30 m",
+            "likes":"2.3k",
+            "title":"Resep Babi Kurma By LastHope Kitchen, Bikin Masuk Penjara",
+        }],
+    };
 
-    const [Inputs, setInputs] = useState({
-        image: '',
-        fullName: '',
-        username: '',
-        email: '',
-        website: '',
-        bio:'',
-        sandiLama:'',
-        sandiBaru:'',
-        sandiKonfirmasi:''
-    })
-    const changeHandler = e => {
-        setInputs({...Inputs, [e.target.name]: e.target.value})
-        console.log(e.target.value)
-     }
-     const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const dataProfile = {
-            image: Inputs.image,
-            fullName: Inputs.fullName,
-            username: Inputs.username,
-            email: Inputs.email,
-            website: Inputs.website,
-            bio: Inputs.bio,
-            sandiLama:Inputs.sandiLama,
-            sandiBaru: Inputs.sandiBaru,
-            sandiKonfirmasi: Inputs.sandiKonfirmasi
-          }
-          console.log(dataProfile)
-          notify()
-      }
-
-      
   return (
-    <form
-        onSubmit={handleSubmit} 
-        className='p-5 lg:px-[227px] lg:py-[80px] mt-[80px] font-Outfit'>
-        <div className='mt-5'>
-                {/* PROFILE IMG */}
-            <div className="flex gap-5 items-center">
-                <img 
-                    src="/profile/user-profile.png" 
-                    alt="profile picture"
-                    className='w-[55px] h-[55px] sm:w-[248px] sm:h-[248px] rounded-full' />
-                <input 
-                    type="file" 
-                    name="image" 
-                    id="image" 
-                    onChange={changeHandler}
-                    hidden
-                     />
-                <div>
-                    <label 
-                        htmlFor="image"
-                        className='text-accent-1 texxt-[14px] sm:text-[24px]'
-                        >
-                        Unggah Gambar
-                    </label>
-                    <p className='text-[10px] sm:text-[20px]'>
-                    Foto ukuran 300 x 300 px atau 300 x 400 px, tipe JPG/JPEG/PNG, max 1MB <br />
-                    Foto harus memiliki ukuran rasio 1:1
-                    </p>
-                    <p>{Inputs.image[0]}</p>
+    <div className='xl:px-36'>
+ 
+        <div className='mt-28 md:mt-26 lg:mt-26 sm:px-4'>
+              {/* PROFILE IMG */}
+              <div className="flex justify-between px-4 md:px-1">
+                <div className='flex gap-5'>
+                    <img 
+                        src="/profile/user-profile.png" 
+                        alt="profile picture"
+                        className='w-[55px] h-[55px] sm:w-[121px] sm:h-[121px] rounded-full' />
+                
+                    <div className="flex lg:justify-between xl:justify-between  ">
+                        <div className="flex flex-col">
+                                    {/* NAMA USER */}
+                                    <p className='text-[10px] sm:text-[32px] font-semibold'>
+                                    Joseph Indera Rabbanni
+                                    </p>
+                                    {/* BIO USER */}
+                                    <p className='text-[6px] sm:text-[16px] w-[262px] xl:w-[768px] md:w-[600px]'>Halo, saya Joseph Indera Rabbani, seorang pecinta kuliner dan penulis resep yang berdedikasi. Dengan lebih dari 1 Abad pengalaman di dapur, saya telah menjelajahi berbagai macam masakan dari seluruh dunia dan menemukan kegembiraan dalam berbagi resep yang lezat dan mudah diikuti.</p>
+                                    {/* SETTING BTN */}
+                                    <div className='text-right'>
+                                        <Link
+                                            to={""} 
+                                            className='w-[35px] h-[10px] sm:w-[99px] sm:h-[33px] text-[6px] sm:text-[16px] border-2 border-black rounded-[27px] sm:px-6 sm:py-2 px-4 py-1 sm:hidden font-medium'>
+                                        Settings
+                                        </Link>
+                                    </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            
+                        <div className=''>
+                                <Link
+                                    to={""} 
+                                    className='w-[35px] h-[10px] sm:w-[99px] sm:h-[33px] text-[6px] sm:text-[16px] border-2 border-black rounded-[27px] sm:px-6 sm:py-2 px-4 py-1 hidden sm:flex items-center'>
+                                   Settings
+                                </Link>
+                            </div>
+                </div>
         </div>
-        <div className='p-2 border-1 sm:hidden'>
-                <hr />
+        <div className="p-5">
+            <hr className=' md:mt-12 mb-4 md:mb-8 ' />
         </div>
-        {/* FORM INPUT PROFILE DATA */}
-        <div 
-            className='sm:mt-10 flex flex-col gap-4'>
-            {/* fullName*/}
-            <div className="flex flex-col gap-1 ">
-                <label 
-                    htmlFor="fullName"
-                    className='sm:font-bold text-[10px] sm:text-[20px]'
-                    >
-                    fullName
-                </label>
-                <input 
-                    type="text" 
-                    name='fullName' 
-                    className='w-[300px] h-[40px] sm:w-[628px] sm:h-[58px] rounded-[6px] border-black border-2 p-1' 
-                    onChange={changeHandler}/>
-            </div>
-            {/* USERNAME */}
-            <div className="flex flex-col gap-1">
-                <label 
-                    htmlFor="username"
-                    className='sm:font-bold  text-[10px] sm:text-[20px]'
-                    >
-                    USERNAME
-                </label>
-                <input 
-                    type="text" 
-                    name='username' 
-                    className='w-[300px] h-[40px] sm:w-[628px] sm:h-[58px] rounded-[6px] border-black border-2 p-1' 
-                    onChange={changeHandler}/>
-            </div>
-            {/* E_MAIL */}
-            <div className="flex flex-col gap-1">
-                <label 
-                    htmlFor="email"
-                    className='sm:font-bold text-[10px] sm:text-[20px]'
-                    >
-                    E-MAIL
-                </label>
-                <input 
-                    type="text" 
-                    name='email' 
-                    className='w-[300px] h-[40px] sm:w-[628px] sm:h-[58px] rounded-[6px] border-black border-2 p-1' 
-                    onChange={changeHandler}/>
-            </div>
-            {/* WEBSITE */}
-            <div className="flex flex-col gap-1">
-                <label 
-                    htmlFor="website"
-                    className='sm:font-bold text-[10px] sm:text-[20px]'
-                    >
-                    WEBSITE
-                </label>
-                <p className='text-[8px] sm:text-[14px]'>
-                    Masukkan "http://" atau "https://" di awal.
+
+        <div className='mx-auto max-w-7xl px-4'>
+            <div className='flex justify-between items-center mb-4 md:mb-8'>
+                {/* TITLE */}
+                <p className="font-semibold text-xs md:text-base">
+                    Resep dari Joseph
                 </p>
-                <input 
-                    type="text" 
-                    name='website' 
-                    className='w-[300px] h-[40px] sm:w-[628px] sm:h-[58px] rounded-[6px] border-black border-2 p-1' 
-                    onChange={changeHandler}/>
-            </div>
-            {/* BIO */}
-            <div className="flex flex-col gap-1">
-                <label 
-                    htmlFor="bio"
-                    className='sm:font-bold text-[10px] sm:text-[20px]'
-                    >
-                    BIO
-                </label>
-                <p className='text-[8px] sm:text-[14px] w-[300px] h-[40px] sm:w-[628px] sm:h-[58px]'>
-                    Tambahkan biodata singkat untuk memberi tahu komunitas KC lebih banyak tentang diri Anda. Apa makanan favorit Anda? Mengapa Anda suka memasak? Kami ingin tahu!
-                </p>
-                <textarea 
-                    type="text" 
-                    name='bio' 
-                    className='w-[300px] h-[181px] sm:w-[627px] sm:h-[288px] rounded-[6px] border-black border-2 p-1' 
-                    onChange={changeHandler}/>
-            </div>
-            {/* UBAH KATA SANDI */}
-            <p className='font-bold text-[10px] sm:text-[20px]'>UBAH KATA SANDI <br />
-                <span className='text-[8px] sm:text-[14px] font-normal'>Kata sandi Anda harus terdiri dari setidaknya 8 karakter.</span>
-            </p>
-            {/* KATA SANDI LAMA */}
-            <div className="flex flex-col gap-1">
-                <label 
-                    htmlFor="sandiLama"
-                    className=' text-[8px] sm:text-[14px]'
-                    >
-                    KATA SANDI LAMA
-                </label>
-                <input 
-                    type="text" 
-                    name='sandiLama' 
-                    className='w-[150px] h-[25px] sm:w-[304px] sm:h-[57px] rounded-[6px] border-black border-2 p-1' 
-                    onChange={changeHandler}/>
-            </div>
-            {/* KATA SANDI BARU DAN KONFIRMASI KATA SANDI BARU */}
-            <div className="flex gap-4">
-                {/* KATA SANDI BARU */}
-            <div className="flex flex-col gap-1">
-                <label 
-                    htmlFor="sandiBaru"
-                    className=' text-[8px] sm:text-[14px]'
-                    >
-                    KATA SANDI BARU
-                </label>
-                <input 
-                    type="text" 
-                    name='sandiBaru' 
-                    className='w-[150px] h-[25px] sm:w-[304px] sm:h-[57px] rounded-[6px] border-black border-2 p-1' 
-                    onChange={changeHandler}/>
-            </div>
-            {/* KATA SANDI BARU KONFIRMASI */}
-            <div className="flex flex-col gap-1">
-                <label 
-                    htmlFor="sandiKonfirmasi"
-                    className=' text-[8px] sm:text-[14px]'
-                    >
-                    KONFIRMASI KATA SANDI BARU
-                </label>
-                <input 
-                    type="text" 
-                    name='sandiKonfirmasi' 
-                    className='w-[150px] h-[25px] sm:w-[304px] sm:h-[57px] rounded-[6px] border-black border-2 p-1'
-                    onChange={changeHandler} />
-            </div>
-            
-            </div>
-            <div className="text-right sm:px-[180px] px-[18px]">
-                <button 
-                type="submit"
-                className="bg-[#399B0A] rounded-[27px] w-[100px] h-[20px] sm:w-[181px] sm:h-[36px] text-white font-bold text-[10px] sm:text-[16px] ">
-                    Simpan Perubahan
+                
+                {/* ADD RECIPE BUTTON */}
+                <button className='bg-black rounded-full text-xs md:text-base text-white px-3 md:px-6 py-1 md:py-2'>
+                    Tambah Resep
                 </button>
             </div>
+
+            {/* RECIPE CARDS */}
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-5 mb-16'>
+                {Object.keys(data_kartu).map((key, i)=>(
+                    data_kartu[key].map((item, j) => (
+                        <div key={i + '-' + j}>
+                            <CardProfile
+                                image={item.image}
+                                time={item.time}
+                                likes={item.likes}
+                                tittle={item.title}
+                                creatorName={item.nama}
+                                creatorImage={item.user}
+                            />
+                        </div>
+                    ))
+                ))}
+            </div>
         </div>
-       
-    </form>
+    </div>
   )
 }
 
-export default Profile
+export default Profile;
