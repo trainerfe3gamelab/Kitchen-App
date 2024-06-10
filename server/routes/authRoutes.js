5f3579f8e124487df84572f721065f360695054e
Galang Arsandy <galangarsandy@gmail.com>
2024-06-10 22:25:19 +0700

    Add: endpoint verify auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { loginUser, logoutUser } = require("../controllers/authController");
+const { authenticate } = require("../middleware/auth");
 
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.get("/",authenticate, (req, res) => {
+    res.json(req.user);
+})
 
 module.exports = router;
