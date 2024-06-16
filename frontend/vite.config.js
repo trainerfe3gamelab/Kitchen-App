0f9083248ebf098514f1bf2838cad974ecaed277
trainerfe3gamelab <trainer.fe3.gamelab@gmail.com>
2024-06-16 16:11:09 +0700

    ganti port menjadi 4000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // atau 'localhost'
+    port: 4000
+  },
   plugins: [react()],
 })
