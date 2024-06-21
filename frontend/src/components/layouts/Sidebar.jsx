43d2cc086b2fb6da7be71ff10232857bcf353b5f
trainerfe3gamelab <trainer.fe3.gamelab@gmail.com>
2024-06-21 08:13:14 +0700

    tambah sidebar

---
+++ frontend/src/components/layouts/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+        {/* Membuat sidebar */}
+      <div>
+        Sidebar
+      </div>
+
+        {/* Akhir Sidebar */}
+
+
+        <div className='p-7 text2xl font-semibold'>
+            Home page
+        </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
