102ace8a0301467e3ca0ad25521c0c6b7782baac
Galang Arsandy <galangarsandy@gmail.com>
2024-06-24 02:59:25 +0700

    edit: baseUrl & add: vercel config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ import User from "./pages/User";
 import { useLocation } from "react-router-dom";
 import EditRecipe from "./pages/EditRecipe";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://api-msib-6-kitchen-app-04.educalab.id/api";
 axios.defaults.withCredentials = true;
 
 function App() {
